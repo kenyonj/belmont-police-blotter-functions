98442cc4b2fd32934d769b58dd9e92724261e3b1
Justin Kenyon <kenyonj@gmail.com>
2021-12-22 15:01:44 +0100

    update functions for filtering by date and street

--- api/index.rb
+++ api/index.rb
@@ -1,10 +1,46 @@
+require "faraday"
+require "json"
+require "date"
+
+INCIDENTS_URL = "http://justinkenyon.com/belmont-police-blotter/incidents.json"
+
 Handler = Proc.new do |req, res|
 	res.status = 200
-	res['Content-Type'] = 'text/text; charset=utf-8'
-	if req.query.has_key?("name")
-		name = req.query["name"]
-		res.body = "Hello, #{name}!"
+	res["Content-Type"] = "text/json; charset=utf-8"
+
+	filtered_incidents = if req.query.has_key?("month_number")
+		month_number = req.query["month_number"]
+    incidents_by_month(month_number)
+  elsif req.query.has_key?("street")
+		street = req.query["street"]
+    incidents_by_month(street)
 	else
-		res.body = "Hello, stranger!"
+    incidents
 	end
+
+  res.body = filtered_incidents.to_json
+end
+
+def incidents
+  JSON.parse(Faraday.get(INCIDENTS_URL).body)
+end
+
+def incidents_by_month(month_number)
+  all_incidents = incidents
+
+  all_incidents.merge(
+    "items" => all_incidents["items"].select do |incident|
+      DateTime.parse(incident["date"]).month == month_number
+    end
+  )
+end
+
+def incidents_by_street(street)
+  all_incidents = incidents
+
+  all_incidents.merge(
+    "items" => all_incidents["items"].select do |incident|
+      incident["location"].downcase.include?(street.downcase)
+    end
+  )
 end
